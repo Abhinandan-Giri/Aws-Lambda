150e9e41d21c4a7f529d08354c293df02b6fec9d
abhinandankumar_giri <abhinandankumar_giri@epam.com>
2025-03-12 00:00:26 +0530

    task04

--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,9 +1,5 @@
 exports.handler = async (event) => {
-    console.log("SQS Event Received:", JSON.stringify(event, null, 2));
-
-        event.Records.forEach(record => {
-            console.log("SQS Message Body:", record.body);
-        });
+    // TODO implement
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
